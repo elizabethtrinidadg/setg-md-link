9fc7fe0d4586c7fade4866278ce90fbb75ec0236
trinitysilvia <trinitysilviag@gmail.com>
2019-11-04 17:50:27 -0600

    creando funcion iniciar(), para indicar el encabezado de la cli

--- pruebas/prueba.js
+++ pruebas/prueba.js
@@ -5,9 +5,21 @@ const chalk = require('chalk');
 const figlet = require('figlet');
 const inquirer = require('inquirer');
 
+
+const iniciar = () => {
+    console.log(
+        chalk.green.green(
+            figlet.textSync('Prueba CLI'), {
+                font: 'Bubble',
+                horizontalLayout: 'default',
+                verticalLayout: "default"
+            })
+        );
+}
+
 const ejecutar = async() => {
 //mostrar informacion de la libreria en la cabecera
-
+iniciar();
 // preguntas necesarias para crear fichero (nombre, y extension)
 
 //crear fichero
