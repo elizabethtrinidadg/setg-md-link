2a2ec14c8a89ea7313cd5cf0a0fe70a2ea5f641d
trinitysilvia <trinitysilviag@gmail.com>
2019-11-04 17:44:37 -0600

    definiendo pseucodigo para determinar pasos a seguir

--- pruebas/prueba.js
+++ pruebas/prueba.js
@@ -4,3 +4,15 @@ const shelljs = require('shelljs');
 const chalk = require('chalk');
 const figlet = require('figlet');
 const inquirer = require('inquirer');
+
+const ejecutar = async() => {
+//mostrar informacion de la libreria en la cabecera
+
+// preguntas necesarias para crear fichero (nombre, y extension)
+
+//crear fichero
+
+//añadimos mensaje de creacion correcta del nuevo fichero
+};
+
+ejecutar();
