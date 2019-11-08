0062afcb13c67a5b538799b222417b9b92f0a2bb
trinitysilvia <trinitysilviag@gmail.com>
2019-11-08 08:48:07 -0600

    agregando carpeta pruebas a ignore

--- pruebas/prueba.js
+++ pruebas/prueba.js
@@ -5,6 +5,13 @@ const chalk = require('chalk');
 const figlet = require('figlet');
 const inquirer = require('inquirer');
 
+
+//4
+ficheroOk = filepath => {
+    console.log(
+        chalk.white.bgGreen.bold(`Tu fichero ha sido creado correctamente en el directorio  ${filepath}`));
+};
+
 //3
 const crearFichero = (nombreFichero, extension) => {
     const pathFichero = `${process.cwd()}/${nombreFichero}.${extension}`;
@@ -24,7 +31,7 @@ const hacerPreguntas = () => {
             name: 'EXTENSION',
             type: 'list',
             message: '¿Qué extensión tendrá tu nuevo fichero?',
-            choices: ['.js', '.ts', '.java', '.rb', '.kt', '.php', '.html', '.css'],
+            choices: ['.js', '.md', '.java', '.rb', '.kt', '.php', '.html', '.css'],
             filter: function(val) {
                 return val.split('.')[1];
             }
@@ -56,6 +63,7 @@ console.log(respuestas);
 //crear fichero
 const pathFichero = crearFichero(FICHERO, EXTENSION);
 //añadimos mensaje de creacion correcta del nuevo fichero
+ficheroOk(pathFichero);
 };
 
 ejecutar();
